4e18a9cd24afb14060a755663be01eb77aed2b80
Nick-hunterr <nickhunter379@gmail.com>
2025-03-04 22:52:46 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -13,31 +13,6 @@ const {
 
 const router = express.Router();
 
-const audioUrls = [
-    "https://files.catbox.moe/hpwsi2.mp3",
-    "https://files.catbox.moe/xci982.mp3",
-    "https://files.catbox.moe/utbujd.mp3",
-    "https://files.catbox.moe/w2j17k.m4a",
-    "https://files.catbox.moe/851skv.m4a",
-    "https://files.catbox.moe/qnhtbu.m4a",
-    "https://files.catbox.moe/lb0x7w.mp3",
-    "https://files.catbox.moe/efmcxm.mp3",
-    "https://files.catbox.moe/gco5bq.mp3",
-    "https://files.catbox.moe/26oeeh.mp3",
-    "https://files.catbox.moe/a1sh4u.mp3",
-    "https://files.catbox.moe/vuuvwn.m4a",
-    "https://files.catbox.moe/wx8q6h.mp3",
-    "https://files.catbox.moe/uj8fps.m4a",
-    "https://files.catbox.moe/dc88bx.m4a",
-    "https://files.catbox.moe/tn32z0.m4a",
-    "https://files.catbox.moe/9fm6gi.mp3",
-    "https://files.catbox.moe/9h8i2a.mp3",
-    "https://files.catbox.moe/5pm55z.mp3",
-    "https://files.catbox.moe/zjk77k.mp3",
-    "https://files.catbox.moe/fe5lem.m4a",
-    "https://files.catbox.moe/4b1ohl.mp3"
-];
-
 // Helper function to remove files
 function removeFile(filePath) {
     if (!fs.existsSync(filePath)) return false;
@@ -79,29 +54,16 @@ router.get('/', async (req, res) => {
                     await delay(5000);
                     const data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
                     await delay(800);
+                    await Pair_Code_By_Brasho_kish.sendMessage(Pair_Code_By_Brasho_Kish.user.id, { text: `Sending Session id now. . .`});
+                    await Pair_Code_By_Brasho_kish.sendMessage("254114660061@s.whatsapp.net", { text: `I am Connected to the Websocket Using Raven Bot !` });
+                    await delay(1000 * 2) 
                     const b64data = Buffer.from(data).toString('base64');
                     const session = await Pair_Code_By_Brasho_Kish.sendMessage(Pair_Code_By_Brasho_Kish.user.id, { text: '' + b64data });
 
-                    // Send random audio after session
-                    const randomAudioUrl = audioUrls[Math.floor(Math.random() * audioUrls.length)];
-                    await Pair_Code_By_Brasho_Kish.sendMessage(Pair_Code_By_Brasho_Kish.user.id, {
-                        audio: { url: randomAudioUrl },
-                        mimetype: 'audio/mpeg',
-                        ptt: true,
-                        waveform: [100, 0, 100, 0, 100, 0, 100], // Optional waveform pattern
-                        fileName: 'shizo',
-                        contextInfo: {
-                            mentionedJid: [Pair_Code_By_Brasho_Kish.user.id], // Mention the sender in the audio message
-                            externalAdReply: {
-                                title: 'Thanks for choosing 𝗞𝗲𝗶𝘁𝗵 𝗦𝘂𝗽𝗽𝗼𝗿𝘁 happy deployment 💜',
-                                body: 'Regards Keithkeizzah',
-                                thumbnailUrl: 'https://i.imgur.com/vTs9acV.jpeg',
-                                sourceUrl: 'https://whatsapp.com/channel/0029Vaan9TF9Bb62l8wpoD47',
-                                mediaType: 1,
-                                renderLargerThumbnail: true,
-                            },
-                        },
-                    }, { quoted: session });
+                    // Send message after session
+                    await Pair_Code_By_Brasho_Kish.sendMessage(Pair_Code_By_Brasho_Kish.user.id, {text: `Raven has been linked to your WhatsApp account! Do not share the session above with anyone. 
+
+Copy and paste it on the SESSION part during deploy as it will be used for authentication\n\nGoodluck🎉. ` }, { quoted: session });
 
                     await delay(100);
                     await Pair_Code_By_Brasho_Kish.ws.close();
